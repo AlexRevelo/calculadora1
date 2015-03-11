dca77049975cf90a9b6be34115fafc603aa6a5b2
JhonRevelo <jhon4380@gmail.com>
2015-03-10 23:11:17 -0500

    graficar

--- calcular.js
+++ calcular.js
@@ -9,7 +9,7 @@ op="no";
 
 function numero(xx) {  
          if (x=="0" || xi==1  ) {  
-            pantalla.innerHTML=xx; 
+            pantalla.innerHTML+=xx; 
             x=xx; 
             }
          else { 
@@ -20,21 +20,24 @@ function numero(xx) {
          }
 function operar(s) {
          igualar(); 
+         pantalla.innerHTML+=s;
          ni=x;
          op=s; 
          xi=1;
          }
-         function igualar() {
+function igualar() {
          if (op=="no") { 
             pantalla.innerHTML=x;	
             }
          else {
+             
             sl=ni+op+x; 
             sol=eval(sl) 
             pantalla.innerHTML=sol 
             x=sol; 
             op="no";
             xi=1; 
+            
             }
         }
         function raizc() {
@@ -67,4 +70,25 @@ function operar(s) {
          op="no"; 
          xi=1;
          }
-         
+        function  lineal(){
+            var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(250,0);
+ctx.lineTo(250,500);
+ctx.moveTo(0,250);
+ctx.lineTo(500,250);
+
+
+var x= -10;
+var x1 =0;
+
+
+
+ctx.moveTo(x1,500);
+ctx.lineTo(500,0);
+x1+=25;
+
+ctx.stroke();
+
+
+        }
